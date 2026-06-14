e1acfe10e05087f3ea870dddb2d402712faf6945
Tharun Sahani <rammilanrammilan675@gmail.com>
2026-06-14 16:38:29 +0530

    Add files via upload

---
+++ assets/groups.py
@@ -0,0 +1,69 @@
+GROUP_CATEGORIES = {
+
+    "Special Interest": [
+        {"id": "last-30-days", "name": "Last 30 Days Launches"},
+        {"id": "stations", "name": "Space Stations"},
+        {"id": "visual", "name": "Brightest Satellites"},
+        {"id": "active", "name": "Active Satellites"},
+        {"id": "analyst", "name": "Analyst Satellites"},
+        {"id": "fengyun-1c-debris", "name": "FengYun 1C Debris"},
+        {"id": "iridium-33-debris", "name": "Iridium 33 Debris"},
+        {"id": "cosmos-2251-debris", "name": "Cosmos 2251 Debris"},
+    ],
+
+    "Weather & Earth Resources": [
+        {"id": "weather", "name": "Weather Satellites"},
+        {"id": "resource", "name": "Earth Resources"},
+        {"id": "sarsat", "name": "Search & Rescue"},
+        {"id": "dmc", "name": "Disaster Monitoring"},
+        {"id": "tdrss", "name": "Tracking and Data Relay"},
+        {"id": "argos", "name": "ARGOS Data Collection"},
+        {"id": "planet", "name": "Planet Satellites"},
+        {"id": "spire", "name": "Spire Satellites"},
+    ],
+
+    "Communications": [
+        {"id": "geo", "name": "Active Geosynchronous"},
+        {"id": "gpz", "name": "GEO Protected Zone"},
+        {"id": "gpz-plus", "name": "GEO Protected Zone Plus"},
+        {"id": "intelsat", "name": "Intelsat"},
+        {"id": "ses", "name": "SES"},
+        {"id": "eutelsat", "name": "Eutelsat"},
+        {"id": "telesat", "name": "Telesat"},
+        {"id": "starlink", "name": "Starlink"},
+        {"id": "oneweb", "name": "OneWeb"},
+        {"id": "kuiper", "name": "Kuiper"},
+        {"id": "iridium", "name": "Iridium NEXT"},
+        {"id": "orbcomm", "name": "Orbcomm"},
+        {"id": "globalstar", "name": "Globalstar"},
+        {"id": "satnogs", "name": "SatNOGS"},
+        {"id": "amateur", "name": "Amateur Radio"},
+        {"id": "x-comm", "name": "Experimental Communications"},
+        {"id": "other-comm", "name": "Other Communications"},
+    ],
+
+    "Navigation": [
+        {"id": "gnss", "name": "GNSS"},
+        {"id": "gps-ops", "name": "GPS Operational"},
+        {"id": "glo-ops", "name": "GLONASS Operational"},
+        {"id": "galileo", "name": "Galileo"},
+        {"id": "beidou", "name": "BeiDou"},
+        {"id": "sbas", "name": "Satellite Augmentation Systems"},
+        {"id": "nnss", "name": "Navy Navigation"},
+        {"id": "musson", "name": "Russian LEO Navigation"},
+    ],
+
+    "Scientific & Educational": [
+        {"id": "science", "name": "Space & Earth Science"},
+        {"id": "geodetic", "name": "Geodetic"},
+        {"id": "engineering", "name": "Engineering"},
+        {"id": "education", "name": "Education"},
+    ],
+
+    "Miscellaneous": [
+        {"id": "military", "name": "Military"},
+        {"id": "radar", "name": "Radar Calibration"},
+        {"id": "cubesat", "name": "CubeSats"},
+        {"id": "other", "name": "Other Satellites"},
+    ]
+}
